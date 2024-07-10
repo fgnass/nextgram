1934815d6299805898a129721654c68aece11479
Felix Gnass <fgnass@gmail.com>
2024-07-10 06:47:10 +0000

    ISR

--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -1,7 +1,9 @@
-export const dynamicParams = false;
+export const dynamicParams = true;
+export const dynamic = "error";
+export const revalidate = 60;
 
 export function generateStaticParams() {
-  let slugs = ['1', '2', '3', '4', '5', '6'];
+  let slugs = ['1', '2', '3'];
   return slugs.map((slug) => ({ id: slug }));
 }
 
